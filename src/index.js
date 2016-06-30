2ee857aa3b3eadf69b3252ac4f77c1957d007389
Juan Miguel Rubio <jmrubio42@gmail.com>
2016-06-30 13:47:47 -0500

    Adds min/max item count settings

--- src/index.js
+++ src/index.js
@@ -5,18 +5,19 @@ import 'style.scss';
 
 (function ($) {
   const defaults = {
+    maxItems: Infinity,
+    minItems: 0,
     items: [
       { id: 'item1', description: 'Item 1', defaultCount: 1 },
       { id: 'item2', description: 'Item 2', defaultCount: 0 },
       { id: 'item3', description: 'Item 3', defaultCount: 0 }
     ]
   };
-
   let itemCount = {};
+  let totalItems = 0;
+  let settings;
 
   function createItem (item) {
-    itemCount[item.id] = item.defaultCount;
-
     const $description = $('<div />').html(item.description);
     const $controls = $('<div />');
     const $decrementButton = $('<button>-</button>');
@@ -27,39 +28,50 @@ import 'style.scss';
     $controls.append($decrementButton, $countDisplay, $incrementButton);
     $item.append($description, $controls);
 
+    $decrementButton.click((event) => decrement(item.id, $countDisplay));
+    $incrementButton.click((event) => increment(item.id, $countDisplay));
     $item.click((event) => event.stopPropagation());
-    $decrementButton.click((event) => {
-      event.stopPropagation();
-      decrement(item.id, $countDisplay);
-    });
-    $incrementButton.click((event) => {
-      event.stopPropagation();
-      increment(item.id, $countDisplay);
-    });
 
     return $item;
   }
 
   function increment (id, $countDisplay) {
-    itemCount[id]++;
-    $countDisplay.html(itemCount[id]);
+    let count = itemCount[id];
+
+    if (totalItems < settings.maxItems) {
+      count++;
+      totalItems++;
+    }
+
+    itemCount[id] = count;
+    $countDisplay.html(count);
   }
 
   function decrement (id, $countDisplay) {
-    if (itemCount[id] > 0) {
-      itemCount[id]--;
-      $countDisplay.html(itemCount[id]);
+    let count = itemCount[id];
+
+    if (totalItems > settings.minItems && count > 0) {
+      count--;
+      totalItems--;
     }
+
+    itemCount[id] = count;
+    $countDisplay.html(count);
   }
 
   $.fn.iqDropdown = function (options) {
     const $container = this.children().first();
     const $selection = this.find('.iqdropdown-selection');
     const $menu = $('<ul />').addClass('iqdropdown-menu').appendTo($container);
-    const settings = $.extend({}, defaults, options);
 
-    settings.items.forEach(item => createItem(item).appendTo($menu));
     this.click(() => $menu.toggleClass('show-menu'));
+    settings = $.extend({}, defaults, options);
+
+    settings.items.forEach(item => {
+      itemCount[item.id] = item.defaultCount;
+      totalItems += item.defaultCount;
+      createItem(item).appendTo($menu);
+    });
 
     return this;
   };
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $(".iqdropdown").iqDropdown({
-    
+    minItems: 1,
+    maxItems: 5
   });
 });
