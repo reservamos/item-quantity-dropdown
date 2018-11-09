3faa5c9f22b6d39d98dd092778a3cbda018c267d
Juan Miguel Rubio <jmrubio42@gmail.com>
2018-11-09 16:59:47 -0600

    Structure and code update (#5)
    * scss structure - wip
    
    * refactored styles
    
    * removed images
    
    * added icons on no-config iqdropdown
    
    * updated reset styles
    
    * updated l-container styles
    
    * scss variables refactored
    
    * colors modified
    
    * font-styles mixin refactored
    
    * icons added by default, menu-open class moved to parent
    
    * updated styles
    
    * color default modified
    
    * added close button and styles for display menu on top
    
    * variables updated
    
    * comments removed
    
    * image removed
    
    * typo fix
    
    * added height variables
    
    * package-lock.json deleted
    
    * Updates dependencies
    
    * Adds source maps
    
    * Uses airbnb-eslint-config-base
    
    * Fixes eslint errors
    
    * Adds stylelint
    
    * Fixes eslint and stylelint errors
    
    * Adds support for multiple instances with same selector
    
    * Updates readme
    
    * Updates readme
    
    * Updates demo gif
    
    * Fixes yarn command in readme
    
    * added hover bg-transition on controls
    
    * fix arrow position
    
    * Build the package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 /* global jQuery */
 
 // plugin styles
-import 'index.scss';
+import 'styles/main.scss';
 
+/* eslint-disable func-names */
 (function ($) {
   const defaults = {
     maxItems: Infinity,
@@ -11,105 +12,117 @@ import 'index.scss';
     textPlural: 'items',
     controls: {
       position: 'right',
-      incrementText: '+',
-      decrementText: '-',
-      displayCls: 'iqdropdown-item-display',
+      displayCls: 'iqdropdown-content',
       controlsCls: 'iqdropdown-item-controls',
-      counterCls: 'counter'
+      counterCls: 'counter',
     },
     items: {},
     onChange: () => {},
     beforeDecrement: () => true,
-    beforeIncrement: () => true
+    beforeIncrement: () => true,
   };
 
   $.fn.iqDropdown = function (options) {
-    const $selection = this.find('p').last();
-    const $menu = this.find('ul');
-    const $items = $menu.find('li');
-    const settings = $.extend(true, {}, defaults, options);
-    let itemCount = {};
-    let totalItems = 0;
+    this.each(function () {
+      const $this = $(this);
+      const $selection = $this.find('p.iqdropdown-selection').last();
+      const $menu = $this.find('div.iqdropdown-menu');
+      const $items = $menu.find('div.iqdropdown-menu-option');
+      const settings = $.extend(true, {}, defaults, options);
+      const itemCount = {};
+      let totalItems = 0;
 
-    function setItemSettings (id, $item) {
-      const minCount = parseInt($item.data('mincount'), 10);
-      const maxCount = parseInt($item.data('maxcount'), 10);
-
-      settings.items[id] = {
-        minCount: isNaN(minCount) ? 0 : minCount,
-        maxCount: isNaN(maxCount) ? Infinity : maxCount
-      };
-    }
-
-    function addControls (id, $item, updateDisplay) {
-      const $controls = $('<div />').addClass(settings.controls.controlsCls);
-      const $decrementButton = $(`<button>${settings.controls.decrementText}</button>`);
-      const $incrementButton = $(`<button>${settings.controls.incrementText}</button>`);
-      const $counter = $(`<span>${itemCount[id]}</span>`).addClass(settings.controls.counterCls);
-
-      $item.children('div').addClass(settings.controls.displayCls);
-      $controls.append($decrementButton, $counter, $incrementButton);
-
-      if (settings.controls.position === 'right') {
-        $item.append($controls);
-      } else {
-        $item.prepend($controls);
+      function updateDisplay () {
+        const usePlural = totalItems !== 1 && settings.textPlural.length > 0;
+        const text = usePlural ? settings.textPlural : settings.selectionText;
+        $selection.html(`${totalItems} ${text}`);
       }
 
-      $decrementButton.click(event => {
-        const { items, minItems, beforeDecrement, onChange } = settings;
-        const allowClick = beforeDecrement(id, itemCount);
-        if (allowClick && totalItems > minItems && itemCount[id] > items[id].minCount) {
-          itemCount[id]--;
-          totalItems--;
-          $counter.html(itemCount[id]);
-          updateDisplay();
-          onChange(id, itemCount[id], totalItems);
+      function setItemSettings (id, $item) {
+        const minCount = Number($item.data('mincount'));
+        const maxCount = Number($item.data('maxcount'));
+
+        settings.items[id] = {
+          minCount: Number.isNaN(Number(minCount)) ? 0 : minCount,
+          maxCount: Number.isNaN(Number(maxCount)) ? Infinity : maxCount,
+        };
+      }
+
+      function addControls (id, $item) {
+        const $controls = $('<div />').addClass(settings.controls.controlsCls);
+        const $decrementButton = $(`
+          <button class="button-decrement">
+            <i class="icon-decrement"></i>
+          </button>
+        `);
+        const $incrementButton = $(`
+          <button class="button-increment">
+            <i class="icon-decrement icon-increment"></i>
+          </button>
+        `);
+        const $counter = $(`<span>${itemCount[id]}</span>`).addClass(settings.controls.counterCls);
+
+        $item.children('div').addClass(settings.controls.displayCls);
+        $controls.append($decrementButton, $counter, $incrementButton);
+
+        if (settings.controls.position === 'right') {
+          $item.append($controls);
+        } else {
+          $item.prepend($controls);
         }
-        event.preventDefault();
+
+        $decrementButton.click((event) => {
+          const { items, minItems, beforeDecrement, onChange } = settings;
+          const allowClick = beforeDecrement(id, itemCount);
+
+          if (allowClick && totalItems > minItems && itemCount[id] > items[id].minCount) {
+            itemCount[id] -= 1;
+            totalItems -= 1;
+            $counter.html(itemCount[id]);
+            updateDisplay();
+            onChange(id, itemCount[id], totalItems);
+          }
+
+          event.preventDefault();
+        });
+
+        $incrementButton.click((event) => {
+          const { items, maxItems, beforeIncrement, onChange } = settings;
+          const allowClick = beforeIncrement(id, itemCount);
+
+          if (allowClick && totalItems < maxItems && itemCount[id] < items[id].maxCount) {
+            itemCount[id] += 1;
+            totalItems += 1;
+            $counter.html(itemCount[id]);
+            updateDisplay();
+            onChange(id, itemCount[id], totalItems);
+          }
+
+          event.preventDefault();
+        });
+
+        $item.click(event => event.stopPropagation());
+
+        return $item;
+      }
+
+      $this.click(() => {
+        $this.toggleClass('menu-open');
       });
 
-      $incrementButton.click(event => {
-        const { items, maxItems, beforeIncrement, onChange } = settings;
-        const allowClick = beforeIncrement(id, itemCount);
-        if (allowClick && totalItems < maxItems && itemCount[id] < items[id].maxCount) {
-          itemCount[id]++;
-          totalItems++;
-          $counter.html(itemCount[id]);
-          updateDisplay();
-          onChange(id, itemCount[id], totalItems);
-        }
-        event.preventDefault();
+      $items.each(function () {
+        const $item = $(this);
+        const id = $item.data('id');
+        const defaultCount = Number($item.data('defaultcount') || '0');
+
+        itemCount[id] = defaultCount;
+        totalItems += defaultCount;
+        setItemSettings(id, $item);
+        addControls(id, $item);
       });
 
-      $item.click(event => event.stopPropagation());
-
-      return $item;
-    }
-
-    function updateDisplay () {
-      const usePlural = totalItems !== 1 && settings.textPlural.length > 0;
-      const text = usePlural ? settings.textPlural : settings.selectionText;
-      $selection.html(`${totalItems} ${text}`);
-    }
-
-    this.click(() => {
-      $selection.toggleClass('menu-open');
-      $menu.toggleClass('show-menu');
+      updateDisplay();
     });
-
-    $items.each(function () {
-      const $item = $(this);
-      const id = $item.data('id');
-      const defaultCount = parseInt($item.data('defaultcount') || '0', 10);
-
-      itemCount[id] = defaultCount;
-      totalItems += defaultCount;
-      setItemSettings(id, $item);
-      addControls(id, $item, updateDisplay);
-    });
-
-    updateDisplay();
 
     return this;
   };
